5eeb5225253d48d83e4131c56e708889980ccf44
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-01-30 14:07:52 +0300

    stage 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import { program } from 'commander';
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+
 program.parse();
