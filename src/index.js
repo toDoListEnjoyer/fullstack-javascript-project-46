b73cb7bf5f65ce5398b7d567db08d0bb9deab06a
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-03-16 12:16:54 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ const getFileData = (filepath) => {
   return JSON.parse(data);
 };
 
-const getFormat = (filepath) => extname(filepath).substring(1);
+// const getFormat = (filepath) => extname(filepath).substring(1);
 
-const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
+const gendiff = (filepath1, filepath2) => {
   const data1 = getFileData(filepath1);
   const data2 = getFileData(filepath2);
   // const [format1, format2] = [getFormat(filepath1), getFormat(filepath2)];
-  
 
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const keys = _.union(keys1, keys2).sort();
 
   const result = ['{\n'];
-  for (const key of keys) {
+
+  keys.forEach(key => {
     if (!Object.hasOwn(data1, key)) {
       result.push(`  + ${key}: ${data2[key]}\n`);
     } else if (!Object.hasOwn(data2, key)) {
@@ -30,8 +30,9 @@ const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
     } else {
       result.push(`    ${key}: ${data1[key]}\n`);
     }
-  }
+  });
   result.push('}');
+  
   return result.join('');
 };
 
