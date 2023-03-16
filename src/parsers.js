e5f0b960ccb9c764e11d2323d87c434c85f2fadd
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-03-16 13:15:06 +0300

    fix linter errors

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,13 @@
 import YAML from 'js-yaml';
 
 export default (data, format) => {
-    switch (format) {
-      case 'json':
-        return JSON.parse(data);
-      case 'yaml':
-      case 'yml':
-        return YAML.load(data);
-    }
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+    case 'yml':
+      return YAML.load(data);
+    default:
+      throw new Error(`'Unknown format! ${format}'`);
+  }
 };
