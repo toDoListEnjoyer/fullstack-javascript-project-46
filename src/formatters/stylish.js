874a2a93438c3059fe324bbdecd1519173d05994
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-03-22 12:15:19 +0300

    remove console.logs

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,50 +9,24 @@ const stringify = (data, depth, mapping) => {
   }
 
   const output = Object.entries(data)
-    .map(([key, value]) => {
-      console.log(`key ${key}, value ${value}`);
-      return `${mapping.unchanged({ key, value }, depth + 1)}`;
-    });
+    .map(([key, value]) => `${mapping.unchanged({ key, value }, depth + 1)}`);
 
   return ` {\n${output.join('\n')}\n  ${indent(depth)}}`;
 };
 
 const mapping = {
   root: ({ children }, depth, iter) => {
-    console.log('root');
-    console.log(`depth ${depth}\n`);
     const output = children.flatMap((node) => mapping[node.type](node, depth + 1, iter));
-    console.log(output);
     return `{\n${output.join('\n')}\n}`;
   },
   nested: ({ key, children }, depth, iter) => {
-    console.log('nested');
-    console.log(`key ${key} depth ${depth}\n`);
-    const output = children.flatMap((node) => {
-      console.log(`depth ${depth}`);
-      return mapping[node.type](node, depth + 1, iter);
-    });
-    console.log(`output ${output}`);
+    const output = children.flatMap((node) => mapping[node.type](node, depth + 1, iter));
     return `${indent(depth)}  ${key}: {\n${output.join('\n')}\n  ${indent(depth)}}`;
   },
-  added: (node, depth) => {
-    console.log('added');
-    console.log(`node ${JSON.stringify(node, null, 4)} depth ${depth}\n`);
-    return `${indent(depth)}+ ${node.key}:${stringify(node.value, depth, mapping)}`;
-  },
-  deleted: (node, depth) => {
-    console.log('deleted');
-    console.log(`node ${JSON.stringify(node, null, 4)} depth ${depth}\n`);
-    return `${indent(depth)}- ${node.key}:${stringify(node.value, depth, mapping)}`;
-  },
-  unchanged: (node, depth) => {
-    console.log('unchanged');
-    console.log(`node ${JSON.stringify(node, null, 4)} depth ${depth}\n`);
-    return `${indent(depth)}  ${node.key}:${stringify(node.value, depth, mapping)}`;
-  },
+  added: (node, depth) => `${indent(depth)}+ ${node.key}:${stringify(node.value, depth, mapping)}`,
+  deleted: (node, depth) => `${indent(depth)}- ${node.key}:${stringify(node.value, depth, mapping)}`,
+  unchanged: (node, depth) => `${indent(depth)}  ${node.key}:${stringify(node.value, depth, mapping)}`,
   changed: (node, depth) => {
-    console.log('changed');
-    console.log(`node ${JSON.stringify(node, null, 4)} depth ${depth}\n`);
     const { key, value1, value2 } = node;
     const data1 = `${indent(depth)}- ${key}:${stringify(value1, depth, mapping)}`;
     const data2 = `${indent(depth)}+ ${key}:${stringify(value2, depth, mapping)}`;
@@ -62,7 +36,6 @@ const mapping = {
 };
 
 const renderTree = (ast) => {
-  console.log(JSON.stringify(ast, null, 4));
   const iter = (node, depth) => mapping[node.type](node, depth, iter);
   return iter(ast, 0);
 };
