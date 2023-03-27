04cb84ea70ee5158ab1e7a8bcaa3dcb238494902
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-03-27 13:31:09 +0300

    fix errors

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,21 +4,21 @@ const buildDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   return keys.map((key) => {
-    let node;
     if (!_.has(data2, key)) {
-      node = { key, type: 'deleted', value: data1[key] };
-    } else if (!_.has(data1, key)) {
-      node = { key, type: 'added', value: data2[key] };
-    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      node = { key, children: buildDiff(data1[key], data2[key]), type: 'nested' };
-    } else if (!_.isEqual(data1[key], data2[key])) {
-      node = {
+      return { key, type: 'deleted', value: data1[key] };
+    }
+    if (!_.has(data1, key)) {
+      return { key, type: 'added', value: data2[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, children: buildDiff(data1[key], data2[key]), type: 'nested' };
+    }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
         key, type: 'changed', value1: data1[key], value2: data2[key],
       };
-    } else {
-      node = { key, type: 'unchanged', value: data1[key] };
     }
-    return node;
+    return { key, type: 'unchanged', value: data1[key] };
   });
 };
 
